fc045cee7eb29cf7f72f609422546d2c8ee158f6
Muhammad Abdurrohman <abdurrohmanmuhammad123@gmail.com>
2022-06-12 21:45:55 +0700

    Promise marah tidak marah

--- promise.js
+++ promise.js
@@ -1,7 +1,36 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const emosiTheatherIXX = await promiseTheaterIXX();
+  const emosiTheatherVGC = await promiseTheaterVGC();
+
+  return new Promise((resolve, reject) => {
+    let result = 0;
+    if (!emosiTheatherIXX && !emosiTheatherVGC) {
+      return reject(null);
+    } else {
+      if (emosi === "marah") {
+        const IXXmarah = emosiTheatherIXX.filter(
+          (i) => i.hasil === "marah"
+        ).length;
+        const VGCmarah = emosiTheatherVGC.filter(
+          (i) => i.hasil === "marah"
+        ).length;
+        result = IXXmarah + VGCmarah;
+      } else if (emosi === "tidak marah") {
+        const IXXTidakMarah = emosiTheatherIXX.filter(
+          (i) => i.hasil === "tidak marah"
+        ).length;
+        const VGCTidakMarah = emosiTheatherVGC.filter(
+          (i) => i.hasil === "tidak marah"
+        ).length;
+        result = IXXTidakMarah + VGCTidakMarah;
+      }
+      return resolve(result);
+    }
+  });
+};
 
 module.exports = {
   promiseOutput,
